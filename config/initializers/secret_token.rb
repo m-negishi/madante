ed37464302d60330fc0a83b7c3bd09d6c5b0ff09
karamawariken <24karamawariken@gmail.com>
2015-05-06 17:28:33 +0900

    rails入れてみた

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Madante::Application.config.secret_key_base = '8d8c9f81b7b69659948d9a234486174751f25de9167ab8615ebc87b6141021f53fd8a7c2e322d9840ba0780be9971de61ae0797c87e7997d58fb1c7b995c63fd'
